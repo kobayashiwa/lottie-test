2f12cba11ee1421c0320bcdf604e53b305cacc68
kobamov <koba@mov.am>
2020-11-06 13:39:43 +0900

    “jsonデータを読み込み”

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ var animation = bodymovin.loadAnimation({
     renderer: 'svg', // 描画形式
     loop: true, // trueにしたらループ。1回再生の場合はfalse
     autoplay: true, // 自動再生
-    path: 'data.json' // jsonのパスを指定
+    path: '../img/34837-devices-switching.json' // jsonのパスを指定
     name: "Hello World", // 今後のリファレンスとして名前を指定（オプション）
     });
